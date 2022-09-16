7e706d5ec295a81c5a960328fb1ef8d7b5101336
Tim Loh <89267338+timloh-enjinstarter@users.noreply.github.com>
2022-09-16 18:43:31 +0800

    Use own determinisitic deployment proxy

--- src/tasks/deploy_contracts.ts
+++ src/tasks/deploy_contracts.ts
@@ -7,7 +7,7 @@ task("deploy-contracts", "Deploys and verifies Safe contracts")
         await hre.run("deploy")
         await hre.run("local-verify")
         await hre.run("sourcify")
-        await hre.run("etherscan-verify", { forceLicense: true, license: 'LGPL-3.0'})
+        await hre.run("etherscan-verify", { forceLicense: true, license: 'LGPL-3.0', sleep: true })
     });
 
 export { }
